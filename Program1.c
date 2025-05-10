e31feeb96b5c5baac192fe2fba47d189422f5206
Shaileshrsahane <shaileshsahane2005@gmail.com>
2025-05-10 23:07:59 +0530

    Add files via upload

---
+++ Program1.c
@@ -0,0 +1,26 @@
+// write a program which accept one number from user and print that number of even numbers on screen.
+
+#include<stdio.h>
+
+void PrintEven(int iNo)
+{
+    if(iNo <= 0)
+    {
+        return;
+    }
+    for
+    (int i = 1; i <= iNo; i++)
+    {
+        printf("%d ", i * 2);
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number\n");
+    scanf("%d",&iValue);
+    PrintEven(iValue);
+    return 0;
+}
