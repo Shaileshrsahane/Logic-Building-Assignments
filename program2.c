103d3b8bd132cecd5b4cf21120fae70cc706ba5a
SHAILESH R SAHANE <shaileshsahane2005@gmail.com>
2025-09-15 12:57:41 +0530

    Add files via upload

---
+++ program2.c
@@ -0,0 +1,27 @@
+// write a program which accepts numbers from user and display its factors in descending order
+#include<stdio.h>
+
+void FactRev(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = (iNo/2);iCnt >= 1;iCnt--)
+    {
+        if((iNo % iCnt) == 0)
+        {
+            printf(" %d ",iCnt);
+        }
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    FactRev(iValue);
+
+    return 0;
+}
---
+++ program4.c
@@ -0,0 +1,37 @@
+//write a program which accepts number from the user and return summation of all its non factors
+#include<stdio.h>
+
+int SumNonFact(int iNo)
+{
+    int iCnt = 0;
+    int iSum = 0;
+
+    if(iNo < 0) //filter to treat negative number as positive
+    {
+        iNo = -iNo;
+    }
+
+    for(iCnt = 1;iCnt < iNo; iCnt++)
+    {
+        if((iNo % iCnt)!= 0)
+        {
+            iSum = iSum + iCnt;
+        }
+    }
+    return iSum;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    iRet = SumNonFact(iValue);
+
+    printf("%d",iRet);
+
+    return 0;
+}
